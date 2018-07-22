7429be7a326ff347aa89b775c0d5ca0621396929
omelet-dev <tr.pultz@gmail.com>
2018-07-22 19:14:48 -0400

    Adds PostCSS tasks and pipes to Gulp file. PostCSS adds nesting elements, variables, and autoprefixers.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp');
-watch = require('gulp-watch');
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nestedcss = require('postcss-nested');
 
 //CLI: gulp
 gulp.task('default', function(){
@@ -11,9 +15,27 @@ gulp.task('html', function(){
 	console.log("Wicked HTML edits, bro!");
 });
 
+gulp.task('styles', function(){
+	console.log("Sass or PostCSS stuff here.");
+	/* Converting PostCSS to traditional CSS. 
+	Key Terms:
+	gulp.src() = get css
+	gulp.dest() = where the new css should be; Run through PostCSS filters.	
+	
+	gulp.src is async function. We return so gulp is aware of the task finish.
+	*/
+	return gulp.src('./app/assets/styles/styles.css')
+			.pipe(postcss([autoprefixer, cssvars, nestedcss]))
+			.pipe(gulp.dest('./app/temp/styles'));
+});
+
 //Watches for changes to the index.html file.
 gulp.task('watch', function(){
 	watch('./app/index.html', function(){
 		gulp.start('html'); //Trigger our HTML task.
+	});
+	
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('styles');
 	});
 });
