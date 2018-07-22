0105d3dacc4cbe74f455baefa10b950aaeb4e1df
omelet-dev <tr.pultz@gmail.com>
2018-07-22 15:52:29 -0400

    Adding first gulp tasks.

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+
+//CLI: gulp
+gulp.task('default', function(){
+	console.log("Gulp!!!!");
+});
+
+//CLI: gulp html
+gulp.task('html', function(){
+	console.log("Wicked HTML edits, bro!");
+});
