8f43dc1afb430d289f86b127836dcb86afd37b46
omelet-dev <tr.pultz@gmail.com>
2018-07-24 23:13:13 -0400

    Adds PostCSS mixins to aid in media queries and responsive design.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,7 +3,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nestedcss = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
 	console.log("Sass or PostCSS stuff here.");
@@ -15,7 +16,7 @@ gulp.task('styles', function(){
 	gulp.src is async function. We return so gulp is aware of the task finish.
 	*/
 	return gulp.src('./app/assets/styles/styles.css')
-			.pipe(postcss([cssImport, cssvars, autoprefixer, nestedcss]))
+			.pipe(postcss([cssImport, mixins, cssvars, autoprefixer, nestedcss]))
 			.on('error', function(errorInfo){
 				console.log('[WARNING] CSS Syntax Error...');
 				console.log(errorInfo.toString());
