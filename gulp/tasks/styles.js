ea6d0a12b5b5baa0684707975f306a0d6dcc46fc
omelet-dev <tr.pultz@gmail.com>
2018-07-24 22:59:11 -0400

    Organizes Gulp Tasks; Adds Response 'Hero' image.

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nestedcss = require('postcss-nested'),
+cssImport = require('postcss-import');
+
+gulp.task('styles', function(){
+	console.log("Sass or PostCSS stuff here.");
+	/* Converting PostCSS to traditional CSS. 
+	Key Terms:
+	gulp.src() = get css
+	gulp.dest() = where the new css should be; Run through PostCSS filters.	
+	
+	gulp.src is async function. We return so gulp is aware of the task finish.
+	*/
+	return gulp.src('./app/assets/styles/styles.css')
+			.pipe(postcss([cssImport, cssvars, autoprefixer, nestedcss]))
+			.on('error', function(errorInfo){
+				console.log('[WARNING] CSS Syntax Error...');
+				console.log(errorInfo.toString());
+				this.emit('end');
+				//ErrorHandling... Prevents Gulp from freaking out on syntax error.
+			})
+			.pipe(gulp.dest('./app/temp/styles'));
+});
