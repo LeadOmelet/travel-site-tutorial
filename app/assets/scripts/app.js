28d618d63618e58f764241d5740389cae3be1818
omelet-dev <tr.pultz@gmail.com>
2018-07-28 19:06:51 -0400

    Installed Babel, began converting ES5 JS to ES6.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,12 +1,21 @@
 /* blueprints (in JS) usually have capitilized letter. Constructor function. */
 /* Web Pack allows us to better organize JS files. By making sense of NodeJS require function. */
 var $ = require('jquery'); //Webpack can use npm packages too.
-var Person = require('./modules/person');
+//var Person = require('./modules/person');
+import Person from './modules/person'; //ES6 BABY!
+
+alert("WHO?>!?!?!?!?!");
+
+class Adult extends Person {
+	payTaxes() {
+		console.log("Jane paid dem taxes.")
+	}
+}
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
-var jane = new Person("Jane Smith", "green");
+var jane = new Person("Jane Smith", "greeeeen");
 jane.greet();
 
 $("h1").remove();
--- app/assets/scripts/modules/person.js
+++ app/assets/scripts/modules/person.js
@@ -1,9 +1,14 @@
-function Person(fullName, favColor){
-	this.name = fullName;
-	this.favColor = favColor;
-	this.greet = function() {
-		console.log("Sup bitches");
+class Person {
+	constructor(fullName, favColor){
+		this.name = fullName;
+		this.favColor = favColor;
+	}
+	
+	
+	greet() {
+		console.log("Sup bitches. This is es2015... YAY122");
 	}
 }
 
-module.exports = Person;
+//module.exports = Person;
+export default Person; //ES6
