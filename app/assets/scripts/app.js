9d3f1b39b29d8a01a3a940b2551df3d380975131
omelet-dev <tr.pultz@gmail.com>
2018-07-29 11:08:40 -0400

    Finished modal design.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,11 @@
 import MobileMenu from './modules/mobile-menu';
 import RevealOnScroll from './modules/reveal-on-scroll';
 import StickyHeader from './modules/sticky-header';
+import Modal from './modules/modal';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickHeader = new StickyHeader();
+var modal = new Modal();
